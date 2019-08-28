1ab1e0d688f7330ff6faa16f2b8f9da0ae641243
Maryus Martsyalis <martsyalis@gmail.com>
2019-08-27 17:36:16 -0700

    add hooks to retrieve expenses history in History component

--- src/history/History.js
+++ src/history/History.js
@@ -1,10 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { getDailyExpenses } from '../services/Firestore';
 
+function History() {
+  return (
+    <div>
+      <HistoryHero />
+      <Table />
+    </div>
+  );
+}
 
-function History(){
-    return(
-        <div>History</div>
-    )
+function HistoryHero() {
+  return (
+    <div className="center-child">
+      <h1 className="title">History</h1>
+    </div>
+  );
+}
+
+function Table() {
+  const [dailyExpensesHistory, handleExpensesHistory] = useState([]);
+  useEffect(() => {
+    if (dailyExpensesHistory.length) return;
+    getDailyExpenses().then(results => {
+      handleExpensesHistory(results);
+    });
+  });
+  const printExenses = dailyExpensesHistory.map((expense, i) => (
+    <div key={i}>
+      <p>{expense.type}</p>
+    </div>
+  ));
+  return <div>{dailyExpensesHistory.length && printExenses}</div>;
 }
 
 export default History;

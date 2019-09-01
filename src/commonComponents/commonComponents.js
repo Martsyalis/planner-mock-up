3c016299c72985f1ca9ebb0ad18a0adabedb105d
Maryus Martsyalis <martsyalis@gmail.com>
2019-09-01 14:45:11 -0700

    abstract number Input component

--- src/commonComponents/commonComponents.js
+++ src/commonComponents/commonComponents.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Hero({ title }) {
   return (
@@ -8,4 +8,31 @@ function Hero({ title }) {
   );
 }
 
-export { Hero };
+// handle Submit has to take the number as the first parameter
+function NumberInput({ handleSubmit }) {
+  const [number, setNumber] = useState('')
+  return (
+    <div>
+      <div className="center-child">
+        <input
+          autoFocus
+          className="input is-rounded price-input"
+          type="number"
+          placeholder="Rounded Price"
+          value={number}
+          onChange={event => setNumber(event.target.value)}
+        />
+      </div>
+      <div className="center-child">
+        <div
+          className="button is-link is-rounded number-input-button"
+          onClick={() => handleSubmit(number)}
+        >
+          Submit
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export { Hero, NumberInput };

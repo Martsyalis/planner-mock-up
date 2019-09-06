ccf266d1c6429a821cd521d631bad845ebba6a1a
Maryus Martsyalis <martsyalis@gmail.com>
2019-09-06 12:06:54 -0700

    adds listen for auth change function

--- src/app/MyProvider.js
+++ src/app/MyProvider.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import { listenForAuthChange } from '../services/firebase-auth';
 
 export const Context = React.createContext();
 
 function MyProvider(props) {
   const [user, handleUser] = useState({});
+  useEffect(() => {
+    listenForAuthChange(user => {
+      user ? handleUser({ uid: user.uid, email: user.uid }) : handleUser({});
+    });
+  }, []);
 
   return (
     <Context.Provider

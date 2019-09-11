d88db847a2c296144d66c3fc455102306809008e
Maryus Martsyalis <martsyalis@gmail.com>
2019-09-11 16:21:29 -0700

    adds Desktop view notifiction

--- src/app/App.js
+++ src/app/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './Routes';
 import Navbar from '../navbar/Navbar';
 import registerServiceWorker from './registerServiceWorker';
+import { Notification } from '../commonComponents/commonComponents';
 import './App.css';
 
 function App() {
@@ -11,6 +12,11 @@ function App() {
     <Router>
       <MyProvider>
         <Navbar />
+        <Notification
+          timeout={10000}
+          text="This expirience has been designed for Mobile View, Desktop compatability us comming"
+          type="desktop-only is-warning"
+        />
         <Routes />
       </MyProvider>
     </Router>
